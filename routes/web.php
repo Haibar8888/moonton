101cd9dbb6bdbd73049c0b51a2b4a45c27c247cd
lalugaluh <lalugaluhhaibara@gmail.com>
2024-03-09 06:13:03 +0700

    slicing 1 bwa

--- routes/web.php
+++ routes/web.php
@@ -15,14 +15,16 @@ use Inertia\Inertia;
 |
 */
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+// Route::get('/', function () {
+//     return Inertia::render('Welcome', [
+//         'canLogin' => Route::has('login'),
+//         'canRegister' => Route::has('register'),
+//         'laravelVersion' => Application::VERSION,
+//         'phpVersion' => PHP_VERSION,
+//     ]);
+// });
+
+Route::redirect('/', '/login', 301);
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
